c8faf0369df3f7ae352348f23f7776deb26bf7bd
林虹 <linhong@linhongdeMBP.lan>
2019-10-15 22:13:44 +0800

    32. Longest Valid Parentheses python

---
+++ 32.py
@@ -0,0 +1,53 @@
+'''
+@Description: 32. Longest Valid Parentheses
+@Author: linhong
+@Date: 2019-10-15 20:18:22
+@LastEditTime: 2019-10-15 22:12:53
+@LastEditors: Please set LastEditors
+也有dp的做法：
+def longestValidParentheses(self, s):
+        dp, res = [0] * len(s), 0  # 初始化dp、定义最优结果变量
+        for i in range(len(s)):
+            if s[i] == ')':  # 只考虑以')'结尾的子串
+                if i > 0 and s[i - 1] == '(':  # 第一中情况，直接加 2
+                    dp[i] = dp[i - 2] + 2
+                if i > 0 and s[i - 1] == ')':  # 第二种情况，
+                    if i - dp[i - 1] > 0 and s[i - dp[i - 1] - 1] == '(':
+                        if i - dp[i - 1] - 1 > 0:  # 当前合法子串，前面还有子串，的情况
+                            dp[i] = dp[i - 1] + dp[i - dp[i - 1] - 2] + 2
+                        else:  # 当前合法子串，前面--没有--子串，的情况
+                            dp[i] = dp[i - 1] + 2
+                res = max(res, dp[i])  # 更新最长的合法子串
+        return res
+'''
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        medium = [-1]
+        ans = 0
+        
+        for i in range(len(s)):
+            if s[i] == '(':
+                medium.append(i)
+            else:
+                medium.pop()
+                if not medium:
+                    medium.append(i)
+                else:
+                    #print i, medium[-1] 
+                    ans = max(ans, i - medium[-1])
+
+        return ans
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    s = "()(()"
+    #s = ")()())"
+    # s = "()(()"
+    # s = "()(()"
+    ans = sol.longestValidParentheses(s)
+    print ans
