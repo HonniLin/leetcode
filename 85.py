4a73e2fbc811f3962c75420e21f45a58250c1052
linhong <Sotifish@163.com>
2020-02-10 21:16:59 +0800

    Hard 85. Maximal Rectangle

--- 85.py
+++ 85.py
@@ -1,10 +1,16 @@
 '''
 @Author: your name
 @Date: 2020-02-10 19:52:24
-@LastEditTime : 2020-02-10 21:07:22
+@LastEditTime : 2020-02-10 21:16:52
 @LastEditors  : Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /leetcode/85.py
+本题可以转化为84 Largest Rectangle in Histogarm来做
+初始化height = [0, 0 ,0 ....]
+对于每一行，先求出以这一行为x轴的每个柱子的高度，求解时，可以每次基于上一行的值进行更新。然后O(n)的时间求出最大矩形，不断更新全局变量res
+
+作者：Jason_Yuan
+链接：https://www.jianshu.com/p/cffdc94c9c19
 '''
 class Solution(object):
     def maximalRectangle(self, matrix):
