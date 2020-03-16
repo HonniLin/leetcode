fa428bc37acaa688a58d22d42f5393e65b372a1f
linhong <Sotifish@163.com>
2020-03-16 08:38:35 +0800

    Easy 206. Reverse Linked List

---
+++ 206.py
@@ -0,0 +1,56 @@
+
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   206.py
+@Time    :   2020/03/16 08:11:21
+@Author  :   linhong02
+@Desc    :   None
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or head.next == None:
+            return head
+
+        nxt = head.next
+        ptr = head
+        while nxt:
+            cur = nxt
+            nxt = cur.next
+            cur.next = ptr
+            ptr = cur
+        
+        return cur
+
+# way 2: 递归 https://leetcode-cn.com/problems/reverse-linked-list/solution/dong-hua-yan-shi-206-fan-zhuan-lian-biao-by-user74/
+class Solution(object):
+	def reverseList(self, head):
+		"""
+		:type head: ListNode
+		:rtype: ListNode
+		"""
+		# 递归终止条件是当前为空，或者下一个节点为空
+		if(head==None or head.next==None):
+			return head
+		# 这里的cur就是最后一个节点
+		cur = self.reverseList(head.next)
+		# 这里请配合动画演示理解
+		# 如果链表是 1->2->3->4->5，那么此时的cur就是5
+		# 而head是4，head的下一个是5，下下一个是空
+		# 所以head.next.next 就是5->4
+		head.next.next = head
+		# 防止链表循环，需要将head.next设置为空
+		head.next = None
+		# 每层递归函数都返回cur，也就是最后一个节点
+		return cur
