4901d508b8ddc3d194ca77dda87137a00dbe560c
linhong <Sotifish@163.com>
2020-03-29 18:06:33 +0800

    Easy 136. Single Number

--- 136.py
+++ 136.py
@@ -4,7 +4,8 @@
 @File    :   136.py
 @Time    :   2020/03/29 18:03:20
 @Author  :   linhong02
-@Desc    :   None
+@Desc    :   求数组中只出现一次的数,其他数都出现2次
+位操作的异或（^)，他的其中一个属性是，n^n = 0, 0^n = n。只要将所有的数都进行异或就得到出现一次的数。
 """
 
 class Solution(object):
@@ -15,5 +16,5 @@ class Solution(object):
         """
         ans = 0
         for i in nums:
-            ans ^= nums[i]
+            ans ^= i
         return ans
