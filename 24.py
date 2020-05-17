b95c67cbd5ee88d7ace937d91086aad628a889de
linhong <Sotifish@163.com>
2020-05-17 12:44:33 +0800

    Medium 24.Swap Nodes in Pairs

---
+++ 24.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   24.py
+@Time    :   2020/05/17 12:19:39
+@Author  :   linhong02
+@Desc    :   None
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
