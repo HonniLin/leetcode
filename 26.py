8be9ecfbe8d981ef70bbba782282e5459e279487
linhong <Sotifish@163.com>
2020-05-17 13:17:56 +0800

    Easy 26.Remove Duplicates from Sorted Array

---
+++ 26.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   26.py
+@Time    :   2020/05/17 13:13:55
+@Author  :   linhong02
+@Desc    :   给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+"""
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return nums
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
