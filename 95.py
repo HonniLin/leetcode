ac22486ed22c07fd66000591758d33a305aac045
linhong <Sotifish@163.com>
2020-08-21 09:04:17 +0800

    M tree

--- 95.py
+++ 95.py
@@ -18,6 +18,11 @@ class Solution(object):
         """
         :type n: int
         :rtype: List[TreeNode]
+        :I start by noting that 1..n is the in-order traversal for any BST with nodes 1 to n. 
+        So if I pick i-th node as my root, the left subtree will contain elements 1 to (i-1), 
+        and the right subtree will contain elements (i+1) to n. 
+        I use recursive calls to get back all possible trees for left and right subtrees and combine them in all possible ways with the root.
+
         """
         def dfs(left, right):
             if left > right:
