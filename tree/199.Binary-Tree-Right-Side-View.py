d5f93b94ede77c7fdff4c42914b242c33e190672
linhong <Sotifish@163.com>
2020-09-23 00:04:07 +0800

    199

---
+++ tree/199.Binary-Tree-Right-Side-View.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        """
+        :desc 输出树的右视图
+        :way DFS-traverse the tree right-to-left, 
+        add values to the view whenever we first reach a new record depth. 
+        This is O(n).
+        """
+        def collect(node, depth):
+            if node:
+                if depth == len(view):
+                    view.append(node.val)
+                collect(node.right, depth+1)
+                collect(node.left, depth+1)
+        view = []
+        collect(root, 0)
+        return view
